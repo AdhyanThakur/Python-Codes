e0c28fcebc1b2962ca719a0ec38072766291947b
AdhyanThakur <adhyanthakur55@gmail.com>
2026-01-04 16:01:46 +0530

    Add files via upload

---
+++ a_function.py
@@ -0,0 +1,13 @@
+### a function to read a file and count the frequency of each word
+def count_word_frequency(file_path):
+    word_count={}
+    with open(file_path, 'r') as file:
+        for line in file:
+            words=line.split()
+            for word in words:
+                word=word.lower().strip(';/\\".,!?')   
+                word_count[word]=word_count.get(word, 0) + 1
+    return word_count
+file_path= 'txtfile.txt'   
+word_frequency=count_word_frequency(file_path)               
+print(word_frequency)
